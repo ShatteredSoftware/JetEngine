dc1b93d426a04c203c234d6e0f416b8ea83462bf
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2018-11-30 21:25:36 -0700

    FEAT: Add default description to JetEngine command.

--- src/main/java/me/uberpilot/jetengine/JetEngine.java
+++ src/main/java/me/uberpilot/jetengine/JetEngine.java
@@ -14,6 +14,7 @@ public class JetEngine extends JPlugin
     @Override
     protected void preEnable()
     {
+        messages.addMessage(new Message("jetengine_cmd.jetengine.list", "Lists the messages loaded in the JetEngine registry."));
         messages.addMessage(new Message("message_list_item", "&f%s $tc- $sc%s"));
 
         Command list = new Command(this, baseCommand, "list", ((sender, label, args) ->
