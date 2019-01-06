e51528c39ab55bdf32aa6af941d7949f65f81cef
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2019-01-06 00:04:00 -0700

    TEST/FIX: Update Command and its tests to follow best practices. + Bring Command test coverage to 100% * Bump version to 1.1.0 * Break Command initialization into smaller pieces. * Fix late-adding command children * Reorganize Command class.
    Closes #12.

--- src/main/java/me/uberpilot/jetengine/command/Command.java
+++ src/main/java/me/uberpilot/jetengine/command/Command.java
@@ -2,6 +2,7 @@ package me.uberpilot.jetengine.command;
 
 import me.uberpilot.jetengine.JPlugin;
 import me.uberpilot.jetengine.language.Message;
+import me.uberpilot.jetengine.util.JUtilities;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a command and information attached to it.
@@ -17,28 +19,24 @@ import java.util.List;
 @SuppressWarnings("WeakerAccess UnusedDeclaration")
 public class Command extends BukkitCommand implements CommandExecutor
 {
-    private final String messagePath;
     /** Link to the plugin this belongs to. */
-    private JPlugin plugin;
-
+    private final JPlugin plugin;
+    /** Child commands. */
+    private final HashMap<String, Command> children;
+    /** Name of the command. */
+    private final String label;
     /** Parent command. */
     private Command parent;
-
-    /** Child commands. */
-    private HashMap<String, Command> children;
-
-    /** Name of the command. */
-    private String label;
-
     /** Permission to be checked before viewing or executing the command */
     private String permission;
 
-    private String feature_name;
+    private String messagePath;
 
-    /** Description of the command and its functionality. Taken from the plugin's
-     * {@link me.uberpilot.jetengine.language.MessageSet MessageSet} from {@link JPlugin#messages}
-     */
-    private String description;
+    private String descriptionPath;
+
+    private String featurePath;
+
+    private String featureName;
 
     private JCommandExecutor executor;
 
@@ -57,8 +55,11 @@ public class Command extends BukkitCommand implements CommandExecutor
         super(label);
 
         //Check for nulls for plugin and for label.
-        if(plugin == null) throw new IllegalArgumentException("Commands: Parent plugin cannot be null.");
-        if(label == null) throw new IllegalArgumentException("Commands: Label cannot be null.");
+        if(plugin == null)
+            throw new IllegalArgumentException("Commands: Parent plugin cannot be null.");
+        if(JUtilities.isEmptyOrNull(label))
+            throw new IllegalArgumentException("Commands: Label cannot be empty or null.");
+
 
         //Basic value-setting.
         this.plugin = plugin;
@@ -69,28 +70,6 @@ public class Command extends BukkitCommand implements CommandExecutor
 
         this.setAliases(aliases);
 
-        this.permission = (parent != null ? (parent.getPermission() + '.') :
-                (plugin.getName().toLowerCase() + '.')) + label.toLowerCase();
-        messagePath = this.getMessagePath();
-
-
-        //Default handling for feature name.
-        if (!plugin.getMessages().hasMessage(messagePath + ".feature_name"))
-        {
-            this.plugin.getMessages().addMessage(new Message(messagePath + ".feature_name",
-                    "the /" + (parent != null ? parent.getPath(' ') + " " : "") +
-                            this.getLabel() + " command"));
-        }
-        this.feature_name = plugin.getMessages().getMessage(messagePath + ".feature_name");
-
-        //Default handling for description.
-        if (!plugin.getMessages().hasMessage(messagePath + ".description"))
-        {
-            this.plugin.getMessages().addMessage(new Message(messagePath + ".description",
-                    plugin.getName() + " " + this.feature_name.substring(4) + "."));
-        }
-        this.description = plugin.getMessages().getMessage(messagePath + ".description");
-
         //Hook this to the parent.
         if(parent != null)
         {
@@ -98,79 +77,110 @@ public class Command extends BukkitCommand implements CommandExecutor
             this.parent.createHelpCommand();
             aliases.forEach(e -> parent.addChild(e, this));
         }
+
+        init();
     }
 
-    private String getMessagePath()
+    private void init()
     {
-        return (this.parent != null ? this.parent.getMessagePath() + "." :
-                this.plugin.getName().toLowerCase() + "_cmd.") + this.label.toLowerCase();
+        this.permission = defaultPermission();
+        this.messagePath = defaultMessagePath();
+        this.descriptionPath = messagePath + ".description";
+        this.featurePath = messagePath + ".feature_name";
+        this.featureName = defaultFeatureName();
+        this.description = defaultDescription();
     }
 
-    public void addAlias(String name)
+    /**
+     * Initializes the path to messages for this command recursively.<br>
+     *
+     * <b>Example:</b> For a plugin named Example, with a command labeled parent, and a child command labeled <br>
+     * child, the message path for the child would be <code>"example_cmd.parent.child"</code>.
+     *
+     * @return The path to messages for this command.
+     */
+    private String defaultMessagePath()
     {
-        if(this.parent != null)
+        if (this.parent != null)
         {
-            this.parent.addChild(name, this);
+            return this.parent.messagePath + "." + this.label.toLowerCase();
         }
-        this.getAliases().add(name);
-    }
-
-    public void addAliases(String... aliases)
-    {
-        Arrays.stream(aliases).forEach(this::addAlias);
-    }
-
-    private void createHelpCommand()
-    {
-        //Add help automatically if this is a child.
-        if (!this.children.containsKey("help"))
+        else
         {
-            this.plugin.getMessages().addMessage(new Message(messagePath + ".help.description",
-                    "Help for " + this.feature_name + "."), true);
-            this.children.put("help", new Command(this.plugin, this, "help",
-                    (sender, unused1, unused2) -> sendCommandHelp(sender), "?"));
+            return this.plugin.getName().toLowerCase() + "_cmd." + this.label.toLowerCase();
         }
     }
 
-    public String getPath(String separator)
+    /**
+     * Default permission handling.<br><br>
+     *
+     * <b>Example: </b> For a plugin named Example, with a command labeled parent, and a child command labeled <br>
+     * child, the permission for the child would be <code>"example.parent.child"</code>.
+     *
+     * @return The default permission.
+     */
+    private String defaultPermission()
     {
-        return (this.parent != null ? this.parent.getPath(separator) + separator : "") + this.getLabel();
+        if (parent != null)
+        {
+            return parent.permission + '.' + label.toLowerCase();
+        }
+        else
+        {
+            return plugin.getName().toLowerCase() + '.' + label.toLowerCase();
+        }
     }
 
-    public String getPath(char separator)
+
+    /**
+     * Default handling for creating a feature name.<br><br>
+     *
+     * <b>Example: </b> For a plugin named Example, with a command labeled parent, and a child command labeled <br>
+     * child, the default feature name for the child would be <code>"the /parent child command"</code>.
+     *
+     * @return Either a default feature name or the one loaded from the attached
+     * {@link me.uberpilot.jetengine.language.MessageSet MessageSet}.
+     */
+    private String defaultFeatureName()
     {
-        return (this.parent != null ? this.parent.getPath(separator) + separator : "") + this.getLabel();
+        if (!plugin.getMessages().hasMessage(featurePath))
+        {
+            if (parent != null)
+            {
+                this.plugin.getMessages().addMessage(new Message(featurePath, "the /" + (parent.getPath(' ') + " ") + this.getLabel() + " command"));
+            }
+            else
+            {
+                this.plugin.getMessages().addMessage(new Message(featurePath, "the /"  + this.getLabel() + " command"));
+            }
+        }
+        return plugin.getMessages().getMessage(featurePath);
     }
 
-    private void sendNoPermissionMessage(CommandSender sender)
+    /**
+     * Default handling for creating a description.<br><br>
+     *
+     * <b>Example: </b> For a plugin named Example, with a command labeled parent, and a child command labeled <br>
+     * child, the default description for the child would be <code>"Example /parent child command."</code>.
+     *
+     * @return Either a default description or the one loaded from the attached
+     * {@link me.uberpilot.jetengine.language.MessageSet MessageSet}.
+     */
+    private String defaultDescription()
     {
-        plugin.getMessenger().sendErrorMessage(sender, "core.no_permission", this.feature_name);
+        if (!plugin.getMessages().hasMessage(descriptionPath))
+        {
+            this.plugin.getMessages().addMessage(new Message(descriptionPath,
+                    plugin.getName() + " " + this.featureName.substring(4) + "."));
+        }
+        return plugin.getMessages().getMessage(descriptionPath);
     }
 
-    private boolean sendCommandHelp(CommandSender sender)
+    @Override
+    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command,
+                             String label, String[] args)
     {
-        plugin.getMessenger().sendCommandHelp(sender, this);
-        return true;
-    }
-
-    public Command getParent()
-    {
-        return parent;
-    }
-
-    public HashMap<String, Command> getChildren()
-    {
-        return children;
-    }
-
-    public String getLabel()
-    {
-        return label;
-    }
-
-    public String getFeatureName()
-    {
-        return feature_name;
+        return execute(commandSender, label, args);
     }
 
     @Override
@@ -209,25 +219,52 @@ public class Command extends BukkitCommand implements CommandExecutor
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command,
-                             String label, String[] args)
-    {
-        return execute(commandSender, label, args);
-    }
-
     public String getPermission()
     {
         return permission;
     }
 
-    public String getDescription()
+    @Override
+    public String getLabel()
     {
-        return this.plugin.getMessages().getMessage(this.messagePath + ".description");
+        return label;
     }
 
-    public void addChild(String label, Command command)
+    @Override
+    public String getDescription()
     {
-        this.children.put(label, command);
+        return this.plugin.getMessages().getMessage(descriptionPath);
+    }
+
+    @Override
+    public org.bukkit.command.Command setDescription(String description)
+    {
+        this.description = description;
+        plugin.getMessages().set(descriptionPath, description);
+        return this;
+    }
+
+    private void createHelpCommand()
+    {
+        //Add help automatically if this is a child.
+        if (!this.children.containsKey("help"))
+        {
+            this.plugin.getMessages().addMessage(new Message(messagePath + ".help.description",
+                    "Help for " + this.featureName + "."), true);
+            this.children.put("help", new Command(this.plugin, this, "help",
+                    (sender, unused1, unused2) -> sendCommandHelp(sender), "?"));
+        }
+    }
+
+    private void sendNoPermissionMessage(CommandSender sender)
+    {
+        plugin.getMessenger().sendErrorMessage(sender, "core.no_permission", this.featureName);
+    }
+
+    private boolean sendCommandHelp(CommandSender sender)
+    {
+        plugin.getMessenger().sendCommandHelp(sender, this);
+        return true;
     }
 
     public void addChild(Command command)
@@ -240,14 +277,59 @@ public class Command extends BukkitCommand implements CommandExecutor
         for(Command command : commands)
         {
             addChild(command);
+            command.setParent(this);
         }
     }
 
-    @Override
-    public org.bukkit.command.Command setDescription(String description)
+    public void addAlias(String name)
     {
-        this.description = description;
-        plugin.getMessages().set(messagePath + ".description", description);
-        return this;
+        if(this.parent != null)
+        {
+            this.parent.addChild(name, this);
+        }
+        this.getAliases().add(name);
+    }
+
+    public void addChild(String label, Command command)
+    {
+        this.children.put(label, command);
+    }
+
+    public void addAliases(String... aliases)
+    {
+        Arrays.stream(aliases).forEach(this::addAlias);
+    }
+
+    public Command getParent()
+    {
+        return parent;
+    }
+
+    private void setParent(Command command)
+    {
+        this.parent = command;
+        this.init();
+        this.parent.createHelpCommand();
+        getAliases().forEach(e -> parent.addChild(e, this));
+    }
+
+    public Map<String, Command> getChildren()
+    {
+        return children;
+    }
+
+    public String getFeatureName()
+    {
+        return plugin.getMessages().getMessage(featurePath);
+    }
+
+    public String getPath(String separator)
+    {
+        return (this.parent != null ? this.parent.getPath(separator) + separator : "") + this.getLabel();
+    }
+
+    public String getPath(char separator)
+    {
+        return (this.parent != null ? this.parent.getPath(separator) + separator : "") + this.getLabel();
     }
 }
--- src/test/java/me/uberpilot/jetengine/command/TestCommand.java
+++ src/test/java/me/uberpilot/jetengine/command/TestCommand.java
@@ -15,11 +15,15 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(JPlugin.class)
+@SuppressWarnings("WeakerAccess")
 public class TestCommand
 {
     @Mock
@@ -28,6 +32,7 @@ public class TestCommand
     CommandSender root;
     @Mock
     CommandSender noPerm;
+    Messenger messenger;
     private Command command, child, child2, child3;
     private static final String name = "TestPlugin";
 
@@ -37,7 +42,7 @@ public class TestCommand
 
         MessageSet messages = new MessageSet();
 
-        Messenger messenger = new Messenger(plugin, messages);
+        messenger = new Messenger(plugin, messages);
 
         messages.addMessage(new Message("core.prefix", "$sc[$pc" + name + "$sc]"));
         messages.addMessage(new Message("core.no_permission", "$pre &cYou don't have permission to use %s"));
@@ -55,6 +60,7 @@ public class TestCommand
         messages.addMessage(new Message("core.list_and", "and"));
         messages.addMessage(new Message(name.toLowerCase() + "_cmd.test.description", "Gives basic information about " + name + "."));
         messages.addMessage(new Message(name.toLowerCase() + "_cmd.help.description", "Gives a list of commands from " + name + "."));
+        messages.addMessage(new Message(name.toLowerCase() + "_cmd.test.two.feature_name", "the magical command"));
 
         plugin = PowerMockito.mock(JPlugin.class);
         when(plugin.getMessages()).thenReturn(messages);
@@ -73,26 +79,58 @@ public class TestCommand
         child = new Command(plugin, command, "fail", ((sender, label, args) -> false));
 
         child2 = new Command(plugin, null, "one", ((sender, label, args) -> false));
-        child3 = new Command(plugin, null, "two", ((sender, label, args) -> false));
+        child3 = new Command(plugin, null, "two", ((sender, label, args) -> false), "too");
 
         command.addChildren(child2, child3);
+        command.addAliases("test1", "test2");
+        command.addAlias("test3");
+        child.addAlias("failed");
 
         child3.setDescription("Something else");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCommandNulls()
+    {
+        new Command(null, null, "label", (a, b, c) -> false);
+        new Command(plugin, null, null, (a, b, c) -> false);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCommandEmpties()
+    {
+        new Command(plugin, null, "", (a, b, c) -> false);
+    }
+
+    @Test
+    public void testCommandAliases()
+    {
+        Assert.assertEquals("Child has correct alias", Collections.singletonList("failed"), child.getAliases());
+        Assert.assertEquals("Command has correct aliases", Arrays.asList("test1", "test2", "test3"), command.getAliases());
+        Assert.assertTrue("Child alias is linked to parent", command.getChildren().containsKey("failed"));
+    }
+
+    @Test
+    public void testChildData()
+    {
+        Assert.assertEquals("Child has correct path", "test fail", child.getPath(' '));
+        Assert.assertEquals("Child has correct path (String)", "test fail", child.getPath(" "));
+        Assert.assertEquals("Child has correct lineage", command, child.getParent());
+        Assert.assertEquals("Child has correct feature name", "the /test one command", child2.getFeatureName());
+        Assert.assertEquals("Child has correct description", "TestPlugin /test one command.", child2.getDescription());
+        Assert.assertEquals("Child permission is sub-permission of parent", "testplugin.test.fail", child.getPermission());
+        Assert.assertEquals("Child sets description properly", "Something else", child3.getDescription());
+        Assert.assertEquals("Child sets feature name properly", "the magical command", child3.getFeatureName());
     }
 
     @Test
     public void testCommandData()
     {
-        Assert.assertEquals("Implicit Tests", "", "");
         Assert.assertEquals("Command has correct label", "test", command.getLabel());
         Assert.assertEquals("Command has correct children", child, command.getChildren().get("fail"));
         Assert.assertEquals("Command has correct description", "Gives basic information about TestPlugin.", command.getDescription());
         Assert.assertEquals("Command has correct permission", "testplugin.test", command.getPermission());
-        Assert.assertEquals("Child has correct path", "test fail", child.getPath(' '));
-        Assert.assertEquals("Child has correct path (String)", "test fail", child.getPath(" "));
-        Assert.assertEquals("Child has correct lineage", command, child.getParent());
-        Assert.assertEquals("Child permission is sub-permission of parent", "testplugin.test.fail", child.getPermission());
-        Assert.assertEquals("SetDescription", "Something else", child3.getDescription());
+        Assert.assertEquals("Command has correct feature name", "the /test command", command.getFeatureName());
     }
 
     @Test
@@ -106,9 +144,13 @@ public class TestCommand
     public void testExecute()
     {
         Assert.assertTrue("Sender with perms successfully executes", command.execute(root, "test", null));
+        Assert.assertTrue("Sender with perms successfully executes (onCommand)", command.onCommand(root, command, "test", null));
         Assert.assertTrue("Sender with no perms 'successfully' executes", command.execute(noPerm, "test", null));
         Assert.assertFalse("Child commands execute correctly", command.execute(root, "test", new String[]{"fail"}));
+        Assert.assertTrue("Help commands execute correctly", command.execute(root, "test", new String[]{"help"}));
+        Assert.assertTrue("Nonexistent commands do not execute", command.execute(root, "test", new String[]{"blah"}));
         Assert.assertTrue("Sender with no perms 'successfully' executes command that should fail", command.execute(noPerm, "test", new String[]{"fail"}));
+        Assert.assertFalse("Command with no children but with args executes correctly", child2.execute(root, "one", new String[]{"here", "are", "some", "args"}));
     }
 
     @Test(expected = IllegalArgumentException.class)
